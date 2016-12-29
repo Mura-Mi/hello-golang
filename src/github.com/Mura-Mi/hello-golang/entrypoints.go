4dc3995d87c5906cd60de3e6cb0a0ecef71a634e
Takuya "Mura-Mi" Murakami <mura.mi.nantoka@gmail.com>
2016-12-29 17:30:45 +0900

    I've learned multiple results

--- src/github.com/Mura-Mi/hello-golang/entrypoints.go
+++ src/github.com/Mura-Mi/hello-golang/entrypoints.go
@@ -9,7 +9,14 @@ func add(x, y int) int {
   return x + y;
 }
 
+func swap(a, b string) (string, string) {
+  return b, a;
+}
+
 func main() {
   fmt.Println("My favorite number is", math.Pi);
   fmt.Println("2 + 4 = ", add(2, 4));
+
+  a, b := swap("Hello", "World");
+  fmt.Println(a, b);
 }
