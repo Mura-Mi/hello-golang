b374db9048e32242c6a3be870fbab457049f17ef
Takuya "Mura-Mi" Murakami <mura.mi.nantoka@gmail.com>
2016-12-29 17:27:06 +0900

    Learned declaring functions

--- src/github.com/Mura-Mi/hello-golang/entrypoints.go
+++ src/github.com/Mura-Mi/hello-golang/entrypoints.go
@@ -5,6 +5,11 @@ import (
   "math"
 )
 
+func add(x int, y int) int {
+  return x + y;
+}
+
 func main() {
   fmt.Println("My favorite number is", math.Pi);
+  fmt.Println("2 + 4 = ", add(2, 4));
 }
